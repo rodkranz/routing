67bdd04e617072c289829bebaf670624d0e14e4f
Rodrigo Lopes <rodrigo.lopes@olx.com>
2018-06-29 11:58:33 +0100

    update commit

---
+++ request.go
@@ -0,0 +1,5 @@
+package routing
+
+import "github.com/aws/aws-lambda-go/events"
+
+type Response events.APIGatewayProxyResponse
--- routing.go
+++ routing.go
@@ -1,16 +1,22 @@
 package routing
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 	"strings"
 	"unicode"
 
 	"github.com/aws/aws-lambda-go/events"
 )
 
+type EncodeResponseFunc func(interface{}) (Response, error)
+type DecodeRequestFunc func(Request) (interface{}, error)
+
+//type DecodeResponseFunc func(*Response) (interface{}, error)
+//type EncodeRequestFunc func(*Request, interface{}) error
+
 // Dispatcher is the method will invoke when router has been match
-type Dispatcher func(events.APIGatewayProxyRequest) (interface{}, error)
+type Dispatcher func(Request) (interface{}, error)
 
 // DispatchGroup is map of routers available in your lambda.
 type DispatchGroup map[string]Dispatcher
@@ -18,13 +24,16 @@ type DispatchGroup map[string]Dispatcher
 // DispatchTable is map of methods available in your lambda.
 type DispatchTable map[string]DispatchGroup
 
+// Router redirect the correct path to correct dispatcher
 type Router struct {
 	rTable DispatchTable // rTable methods and routers available in you lambda
 }
 
+// New point of router struct
 func New() *Router { return &Router{} }
 
-func (r *Router) Register(method, path string, dispatcher Dispatcher) (*Router) {
+// Register any kind of method and path for dispatcher
+func (r *Router) Register(method, path string, dispatcher Dispatcher) *Router {
 	if r.rTable == nil {
 		r.rTable = make(DispatchTable)
 	}
@@ -38,13 +47,32 @@ func (r *Router) Register(method, path string, dispatcher Dispatcher) (*Router) 
 	return r
 }
 
-func (r *Router) Get(path string, dispatcher Dispatcher) (*Router)    { return r.Register(http.MethodGet, path, dispatcher) }
-func (r *Router) Post(path string, dispatcher Dispatcher) (*Router)   { return r.Register(http.MethodPost, path, dispatcher) }
-func (r *Router) Put(path string, dispatcher Dispatcher) (*Router)    { return r.Register(http.MethodPut, path, dispatcher) }
-func (r *Router) Delete(path string, dispatcher Dispatcher) (*Router) { return r.Register(http.MethodDelete, path, dispatcher) }
-func (r *Router) Option(path string, dispatcher Dispatcher) (*Router) { return r.Register(http.MethodOptions, path, dispatcher) }
+// Get Register router for requests method was GET
+func (r *Router) Get(path string, dispatcher Dispatcher) *Router {
+	return r.Register(http.MethodGet, path, dispatcher)
+}
 
-// Lambda
+// Post Register router for requests method was Post
+func (r *Router) Post(path string, dispatcher Dispatcher) *Router {
+	return r.Register(http.MethodPost, path, dispatcher)
+}
+
+// Put Register router for requests method was Put
+func (r *Router) Put(path string, dispatcher Dispatcher) *Router {
+	return r.Register(http.MethodPut, path, dispatcher)
+}
+
+// Delete Register router for requests method was Delete
+func (r *Router) Delete(path string, dispatcher Dispatcher) *Router {
+	return r.Register(http.MethodDelete, path, dispatcher)
+}
+
+// Option Register router for requests method was Option
+func (r *Router) Option(path string, dispatcher Dispatcher) *Router {
+	return r.Register(http.MethodOptions, path, dispatcher)
+}
+
+// Lambda trigger the events to find router and http verb.
 func (r Router) Lambda(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
 	routingMethod, ok := r.rTable[request.HTTPMethod]
 	if ok == false {
@@ -56,7 +84,7 @@ func (r Router) Lambda(request events.APIGatewayProxyRequest) (events.APIGateway
 		return events.APIGatewayProxyResponse{}, ErrRouterNotFound{Resource: request.Resource}
 	}
 
-	response, err := dispatcher(request)
+	response, err := dispatcher(Request(request))
 	if err != nil {
 		return events.APIGatewayProxyResponse{}, ErrDispatcher{Err: err}
 	}
@@ -67,6 +95,7 @@ func (r Router) Lambda(request events.APIGatewayProxyRequest) (events.APIGateway
 	}, nil
 }
 
+// trim remove spaces from string
 func trim(in string) string {
 	return strings.Map(func(r rune) rune {
 		if unicode.IsSpace(r) {
@@ -75,4 +104,3 @@ func trim(in string) string {
 		return r
 	}, in)
 }
-
