7ec8d1b313ba4bb3d264c4fa3a25e63986089fab
Rodrigo Lopes <rodrigo.lopes@olx.com>
2020-04-27 13:24:54 +0100

    removed dep, added go mod, added example

---
+++ example/hello/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+
+	"github.com/rodkranz/routing"
+)
+
+const (
+	hello = "/hello"
+)
+
+func main() {
+	r := routing.New()
+
+	r.Register(http.MethodGet, hello, HandlerHelloWord)
+
+	lambda.Start(r.Lambda)
+}
+
+func HandlerHelloWord(context routing.Context, proxy routing.RequestProxy) (i interface{}, e error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string("Hello world"),
+	}, nil
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/rodkranz/routing
+
+go 1.14
+
+require (
+	github.com/aws/aws-lambda-go v1.16.0
+	github.com/aws/aws-xray-sdk-go v1.0.0
+)
